14df192652872670d5c2ecbea2ac95638d13429c
asafleve123 <asafleve123@gmail.com>
2022-01-19 16:04:04 +0200

    Add Abtraction to Server

---
+++ PingPong/PingPongServer/Server.Abstract/ServerBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongServer.Server.Abstract
+{
+    public abstract class ServerBase
+    {
+
+        public string IP { get; set; }
+        public int Port { get; set; }
+        protected ServerBase(string iP, int port)
+        {
+            IP = iP;
+            Port = port;
+        }
+        public abstract void Connect(string ip, int Port);
+        public abstract void ListenToSocket();
+        public abstract Task Job();
+
+    }
+}
