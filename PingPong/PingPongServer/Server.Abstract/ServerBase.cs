f5b99949682fde8d09af92c30456c80dd38e5455
asafleve123 <asafleve123@gmail.com>
2022-01-19 16:51:19 +0200

    Add Basic Implemntation to Server

--- PingPong/PingPongServer/Server.Abstract/ServerBase.cs
+++ PingPong/PingPongServer/Server.Abstract/ServerBase.cs
@@ -5,19 +5,19 @@ using System.Threading.Tasks;
 
 namespace PingPongServer.Server.Abstract
 {
-    public abstract class ServerBase
+    public abstract class ServerBase<T>
     {
 
         public string IP { get; set; }
         public int Port { get; set; }
-        protected ServerBase(string iP, int port)
+        protected ServerBase(string ip, int port)
         {
-            IP = iP;
+            IP = ip;
             Port = port;
         }
         public abstract void Connect(string ip, int Port);
         public abstract void ListenToSocket();
-        public abstract Task Job();
+        public abstract Task Job(T client);
 
     }
 }
---
+++ PingPong/PingPongServer/Server.Implements/SocketServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+using PingPongServer.Server.Abstract;
+using System.Collections.Generic;
+using System.Text;
+namespace PingPongServer.Server.Implements
+{
+    public class SocketServer : ServerBase<Socket>
+    {
+        public Socket Listener { get; set; }
+        public SocketServer(string ip, int port) : base(ip, port)
+        {
+        }
+
+        public override void Connect(string ip, int port)
+        {
+
+            IPAddress ipAddr = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+            Listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Listener.Bind(localEndPoint);
+        }
+        public override async void ListenToSocket()
+        {
+            Listener.Listen(10);
+            List<Task> tasks = new List<Task>();
+            while (true)
+            {
+                Socket clientSocket = Listener.Accept();
+                tasks.Add(Job(clientSocket));
+            }
+        }
+
+        public override async Task Job(Socket clientSocket)
+        {
+            while (true)
+            {
+                byte[] bytes = new Byte[1024];
+                string data = null;
+
+                while (true)
+                {
+                    int numByte = clientSocket.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, numByte);
+                    if (data.IndexOf("\n") > -1)
+                        break;
+                }
+                byte[] message = Encoding.ASCII.GetBytes(data);
+                clientSocket.Send(message);
+            }
+        }
+
+    }
+}
