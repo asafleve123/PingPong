40d370301e9104796ea6e15dcf25e04f0fbd1809
asafleve123 <asafleve123@gmail.com>
2022-01-20 12:43:33 +0200

    Add TcpClientConnection Implementation

---
+++ PingPong/PingPongServer/ClientConncetion.Implements/TcpClientConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using PingPongServer.Server.Abstract;
+using System.Linq;
+namespace PingPongServer.ClientConncetion.Implements
+{
+    public class TcpClientConnection : IClientConnection
+    {
+
+        public TcpClient Client { get; set; }
+        public NetworkStream Stream { get; set; }
+        public TcpClientConnection(TcpClient client)
+        {
+            Client = client;
+            Stream = Client.GetStream();
+        }
+        public byte[] Recive()
+        {
+            byte[] bytes = new Byte[1024];
+            int numByte = Stream.Read(bytes, 0, bytes.Length);
+            return bytes.Take(numByte).ToArray();
+        }
+
+        public void Send(byte[] bytes)
+        {
+            Stream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
