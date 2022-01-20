54f76cea6b14cfe2a061460631d89c5480b176d4
asafleve123 <asafleve123@gmail.com>
2022-01-20 12:01:25 +0200

    Add Client Handler implement

---
+++ PingPong/PingPongServer/ClientHandlers/StringClientPingPong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PingPongServer.Server.Abstract;
+namespace PingPongServer.ClientHandlers
+{
+    public class StringClientPingPong : ClientHandlerBase
+    {
+        public StringClientPingPong(IClientConnection clientConnection) : base(clientConnection)
+        {
+        }
+
+        public override Task Job()
+        {
+            while (true)
+            {
+                byte[] bytes = MyClientConnection.Recive();
+                string data = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                Console.WriteLine(data);
+                byte[] message = Encoding.ASCII.GetBytes(data);
+                MyClientConnection.Send(message);
+            }
+        }
+    }
+}
